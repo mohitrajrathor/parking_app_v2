cdfd54f0e649fd4405e0fa7835ab420223817322
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2025-07-23 15:03:36 +0000

    Milestone-VP-MAD2 Responsive-PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Parkly: A parking facility management application',
+          short_name: 'Parkly',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#002fffff',
+          icons: [
+            {
+              src: '/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      }
+    )
+  ],
   server: {
     proxy: {
       '/api_v1': {
